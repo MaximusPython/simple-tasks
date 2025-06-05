43fc69b84d359a3380b805c5edba239f1cedaa05
Maximus <85298325+MaximusPython@users.noreply.github.com>
2025-06-05 09:47:22 +0300

    ts lesson

---
+++ type_script.js
@@ -0,0 +1,8 @@
+var num = 2;
+console.log(num);
+var man = {
+    id: 1,
+    age: 23,
+    name: 'Kolya',
+    firstname: 'Ivanov',
+};
---
+++ type_script.ts
@@ -0,0 +1,18 @@
+const num: number = 2
+console.log(num)
+
+//=================================
+
+type Person = {
+  id: number
+  age: number
+  name: string
+  firstname: string
+}
+
+const man: Person = {
+  id: 1,
+  age: 23,
+  name: 'Kolya',
+  firstname: 'Ivanov',
+}
